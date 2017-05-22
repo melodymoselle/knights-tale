020e8b2f10e72d5dd99c6d45182b66fe5ab483d4
Melmo <melodymoselle@gmail.com>
2017-05-22 00:46:23 -0700

    Minor changes.

--- src/main/java/com/melodymoselle/knightsTale/KnightsTaleMain.java
+++ src/main/java/com/melodymoselle/knightsTale/KnightsTaleMain.java
@@ -7,7 +7,7 @@ public class KnightsTaleMain {
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context =
             new ClassPathXmlApplicationContext(
-                "META-INF/spring/knights.xml");
+                    "META-INF/spring/knights.xml");
         Knight knight = context.getBean(Knight.class);
         knight.embarkOnQuest();
         context.close();
---
+++ src/main/java/com/melodymoselle/knightsTale/Minstrel/Minstrel.java
@@ -0,0 +1,17 @@
+package com.melodymoselle.knightsTale.Minstrel;
+
+import java.io.PrintStream;
+
+public class Minstrel {
+    private PrintStream stream;
+    public Minstrel(PrintStream stream) {
+        this.stream = stream;
+    }
+    public void singBeforeQuest() {
+        stream.println("Fa la la, the knight is so brave!");
+    }
+    public void singAfterQuest() {
+        stream.println("Tee hee hee, the brave knight " +
+                "did embark on a quest!");
+    }
+}
